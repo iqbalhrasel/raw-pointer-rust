0dda28430352183b0058f24e5efc815df35040e6
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2026-03-30 16:35:05 +0600

    manual memory allocation. mini vec impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::{
     basic::rw_ptr_math,
     menual_mem::{men_mem, raw_slice, struct_mem},
+    mini_vec::MiniVec,
 };
 
 mod basic;
@@ -8,5 +9,5 @@ mod menual_mem;
 mod mini_vec;
 
 fn main() {
-    raw_slice();
+    let v = MiniVec::new();
 }
--- src/mini_vec.rs
+++ src/mini_vec.rs
@@ -1,5 +1,53 @@
-pub struct MiniVec<T> {
-    ptr: *mut T,
+use std::{
+    alloc::{Layout, alloc, dealloc},
+    ptr::{self, null_mut},
+};
+
+pub struct MiniVec {
+    ptr: *mut i32,
     len: usize,
     cap: usize,
+}
+impl MiniVec {
+    pub fn new() -> Self {
+        return Self {
+            ptr: null_mut(),
+            len: 0,
+            cap: 0,
+        };
+    }
+
+    pub fn push(&mut self, val: i32) {
+        if self.len == self.cap {
+            let new_cap = if self.cap == 0 { 1 } else { self.cap * 2 };
+            let layout = Layout::array::<i32>(new_cap).unwrap();
+
+            unsafe {
+                let new_ptr = alloc(layout) as *mut i32;
+                if new_ptr.is_null() {
+                    println!("allocation failed");
+                    return;
+                }
+
+                if !self.ptr.is_null() {
+                    std::ptr::copy(self.ptr, new_ptr, self.len);
+                    let old_layout = Layout::array::<i32>(self.cap).unwrap();
+                    dealloc(self.ptr as *mut u8, old_layout);
+                    /*
+                     * dealloc frees raw bytes, not typed values
+                     * That’s why it expects *mut u8.
+                     */
+                }
+
+                self.ptr = new_ptr;
+                self.cap = new_cap;
+            }
+        }
+        unsafe {
+            std::ptr::write(self.ptr.add(self.len), val);
+        }
+        self.len += 1;
+    }
+
+    pub fn get() {}
 }
