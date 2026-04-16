d94d4d4686ff5db5af0805fae1b62f3063337e00
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2026-04-16 15:04:25 +0600

    added ffi_basics.rs, build.rs at root for ffi

---
+++ src/ffi_basics.rs
@@ -0,0 +1,4 @@
+unsafe extern "C" {
+    // pub fn abs(x: i32) -> i32;
+    pub fn add(a: i32, b: i32) -> i32;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use crate::{
-    basic::{node_rw, rw_ptr_math, rw_slice},
-    menual_mem::{men_mem, raw_slice, struct_mem},
-    mini_linkedlst::LinkedList,
-    mini_vec::MiniVec,
-};
+use crate::ffi_basics::add;
 
 mod basic;
+mod ffi_basics;
 mod menual_mem;
 mod mini_linkedlst;
 mod mini_vec;
 
 fn main() {
-    rw_slice();
+    let n = unsafe { add(52, 30) };
+
+    println!("{}", n);
 }
